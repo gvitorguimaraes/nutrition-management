5340fab4b1da458775c37307fcf99fa027568099
gvitor <gabriel1.vlguimaraes@gmail.com>
2025-10-16 08:05:32 -0300

    feat: api-gateway service and auth-service setup

---
+++ authservice/src/test/java/br/dev/gvitorguimaraes/nutrition/authservice/AuthserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.gvitorguimaraes.nutrition.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
